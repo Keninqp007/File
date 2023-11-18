1b9a358d17b426fd7d77b7c897a22de27a401752
Keninqp007 <101650138+Keninqp007@users.noreply.github.com>
2023-11-18 19:15:31 +0530

    route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("SHERLOCK Has Been Online" <p><a href="https://t.me/sherlockhash_bot?start=Z2V0LTMxMDYwOTk4NzYxMDA4LTU1ODA5NjAwOTk5NjE3Ng">Visit W3Schools.com!</a></p>)
+    return web.json_response("SHERLOCK Has Been Online")
